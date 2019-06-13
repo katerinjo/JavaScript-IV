0cb3c462a5282541272cd4ca2d3f7e4ec95d792c
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-06-13 13:03:21 -0700

    instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,3 +11,20 @@ class Person {
         console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
     }
 }
+
+class Instructor extends Person {
+    constructor(attrs) {
+        super(attrs)
+        this.specialty = attrs.specialty
+        this.favLanguage = attrs.favLanguage
+        this.catchPhrase = attrs.catchPhrase
+    }
+
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`)
+    }
+
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`)
+    }
+}
